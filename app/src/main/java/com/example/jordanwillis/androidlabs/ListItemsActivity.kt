cc19062526361f57e792fca5dca7af3523dbe615
will0684 <jordan.walcaraz@gmail.com>
2018-09-17 12:26:42 -0400

    Added 3 activities for Lab 2

---
+++ app/src/main/java/com/example/jordanwillis/androidlabs/ListItemsActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jordanwillis.androidlabs
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ListItemsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_items)
+    }
+}
